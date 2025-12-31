0f05173ad730c96d296ad341fd57c2d4ee3c69c4
MahiSharma05 <sharmama0905@gmail.com>
2025-12-31 10:44:06 +0530

    [Mahi Sharma] Add. Leet code question completed.

---
+++ core-java-practice/leet-code-codebase/TwoSum.java
@@ -0,0 +1,16 @@
+// 1.TWO SUM
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j <nums.length;j++){
+                if(nums[i] + nums[j] ==target){
+                    // ans[0]=i;
+                    // ans[1] = j;
+                    return new int [] {i,j};
+                }
+            }
+        }
+        return new int [] {};
+    }
+}
