8c29600e8ae2717af7717f7f9beb3c81732df771
MahiSharma05 <sharmama0905@gmail.com>
2025-12-31 10:20:12 +0530

    [Mahi Sharma] refactor: minimal changes

--- core-java-practice/grc-codebase/programming-elements/EarthVolume.java
+++ core-java-practice/grc-codebase/programming-elements/EarthVolume.java
@@ -1,13 +1,13 @@
 //Write a Program to compute the volume of Earth in km^3 and miles^3
 
-class EarthVolume{
+public class EarthVolume{
     public static void main(String[] args){
-        double radiusInKm = 6378;
-        double pi = 3.14;
-        double volumeInKm = (4/3)* pi* radiusInKm *radiusInKm* radiusInKm;
-                 // Convert km³ to miles³
-        double radiusInMiles = radiusInKm * 0.621;
-        double volumeInMiles = (4/3)* pi *radiusInMiles* radiusInMiles* radiusInMiles;
-        System.out.println("The volume of earth in cubic kilometers is " + volumeInKm +" and cubic miles is " + volumeInMiles);
-    }
+	    int earthRadiusInKm = 6378;
+		final double PI = 3.14;
+		double earthVoulmeInKm = (4 / 3) * PI * earthRadiusInKm * earthRadiusInKm * earthRadiusInKm;
+		double kmToMiles = 1.6; // 1km = 1.6 miles
+		double earthVoulmeInMiles = earthVoulmeInKm * kmToMiles * kmToMiles * kmToMiles ; 
+		System.out.println("The volume of earth in cubic kilometers is " + earthVoulmeInKm + " and cubic miles is " + earthVoulmeInMiles);
+	}
 }
+		
